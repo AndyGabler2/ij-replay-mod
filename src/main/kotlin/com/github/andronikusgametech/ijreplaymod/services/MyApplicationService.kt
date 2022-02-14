61a9bb488b0e055019cb51e37660a875805c3b80
GitHub Action <action@github.com>
2022-02-14 02:28:42 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/andronikusgametech/ijreplaymod/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.andronikusgametech.ijreplaymod.services
+
+import com.github.andronikusgametech.ijreplaymod.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/andronikusgametech/ijreplaymod/services/MyProjectService.kt
+++ src/main/kotlin/com/github/andronikusgametech/ijreplaymod/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.andronikusgametech.ijreplaymod.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.andronikusgametech.ijreplaymod.MyBundle
 
 class MyProjectService(project: Project) {
 
